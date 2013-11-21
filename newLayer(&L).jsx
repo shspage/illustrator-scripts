fcd57de8200616397ee4e66121ec8e61b1801657
shspage <jbtfwyg@yahoo.co.jp>
2013-11-21 22:30:50 +0900

    now generates new color from HUEs

--- newLayer(&L).jsx
+++ newLayer(&L).jsx
@@ -12,37 +12,118 @@
 // This script is distributed under the MIT License.
 // See the LICENSE file for details.
 
-// RGB values for new layer
-var colors = [
-    [0, 104, 221], // blue
-    [220, 0, 165], // magenta
-    [0, 166, 69], // green
-    [244, 127, 0], // orange
-    [159, 82, 197], // purple
-    [211, 39, 0] // red
-    ];
-
 main();
 function main(){
     if(documents.length < 1) return;
 
     var alay = activeDocument.activeLayer;
 
-    // create new color according to the number of layers
-    var color = new RGBColor();
-    var index = activeDocument.layers.length % colors.length;
-    
-    color.red = colors[index][0];
-    color.green = colors[index][1];
-    color.blue = colors[index][2];
-
     // add new layer
-    var lay = activeDocument.layers.add();
-    lay.color = color;
+    var lay = alay.parent.layers.add();
+    lay.color = getNewLayerColor(alay);
     lay.move(alay, ElementPlacement.PLACEBEFORE);
 
     // dummy operation to select new layer
     var rect = lay.pathItems.rectangle(0,0,1,1);
     rect.selected = true;
     rect.remove();
+}
+
+/** getNewLayerColor : Returns a color for new layer
+ * @param alay activeLayer
+ * @returns RGBColor
+ */
+function getNewLayerColor(alay){
+    // various constants
+    var conf = {
+        MIN_HUE_DIFF : 60,
+        HUE_OFFSET : 60,
+        FIXED_SAT : 0.85,
+        FIXED_VAL : 0.85
+        };
+    
+    var colorA = alay.color;
+    var colorB = colorA;
+    var z = alay.zOrderPosition;
+    var layLen = alay.parent.layers.length;
+    if(z != layLen){
+        colorB = alay.parent.layers[layLen - z - 1].color;
+    }
+    
+    var hsvA = rgb2hsv(colorA);
+    var hsvB = rgb2hsv(colorB);
+
+    var h = (hsvA.h + conf.HUE_OFFSET) % 360;
+    if(Math.abs(hsvB.h - h) < conf.MIN_HUE_DIFF){
+        h = (h + conf.HUE_OFFSET) % 360;
+    }
+    
+    var hsv = {h:h, s:conf.FIXED_SAT, v:conf.FIXED_VAL};
+    return hsv2rgb( hsv );
+}
+
+/** hsv2rgb : Converts HSV to RGBColor
+ * @param hsv { h : 0-360, s,v : 0-1 }
+ * @returns RGBColor
+ */
+function hsv2rgb(hsv) { // hsv { h : 0-360, s,v : 0-1 }
+    var rgb = {r:0, g:0, b:0};
+    var m = 255;
+    // r,g,b : 0-m
+    
+    var Hi = Math.floor(hsv.h / 60) % 6;
+    var f = hsv.h / 60 - Hi;
+    var p = Math.round(hsv.v * (1 - hsv.s) * m);
+    var q = Math.round(hsv.v * (1 - f * hsv.s) * m);
+    var t = Math.round(hsv.v * (1 - (1 - f) * hsv.s) * m);
+
+    hsv.v = Math.round(hsv.v * m);
+    hsv.s = Math.round(hsv.s * m);
+    
+    switch(Hi){
+    case 0: rgb.r = hsv.v; rgb.g = t; rgb.b = p; break;
+    case 1: rgb.r = q; rgb.g = hsv.v; rgb.b = p; break;
+    case 2: rgb.r = p; rgb.g = hsv.v; rgb.b = t; break;
+    case 3: rgb.r = p; rgb.g = q; rgb.b = hsv.v; break;
+    case 4: rgb.r = t; rgb.g = p; rgb.b = hsv.v; break;
+    case 5: rgb.r = hsv.v; rgb.g = p; rgb.b = q; break;
+    }
+    //return rgb; // for general purpose
+
+    var c = new RGBColor();
+    c.red = rgb.r;
+    c.green = rgb.g;
+    c.blue = rgb.b;
+    return c;
+}
+
+/** rgb2hsv : Converts RGBColor to HSV
+ * @param rgb RGBColor { red,green,blue : 0-255 }
+ * @returns hsv { h: 0-360, s,v: 0-1 }
+ */
+function rgb2hsv(rgb) {  // rgb { red, green, blue : 0-255 }
+    var hsv = {h:0, s:0, v:0};
+    // h: 0-360, s,v: 0-1
+    
+    var mx = Math.max(Math.max(rgb.red, rgb.green), rgb.blue);
+    var mn = Math.min(Math.min(rgb.red, rgb.green), rgb.blue);
+    
+    if(mx > 0){
+        hsv.v = mx / 255;
+        
+        hsv.s = (mx - mn) / mx;
+        
+        if(mx != mn){
+            if(mx == rgb.red){
+                hsv.h = 60 * (rgb.green - rgb.blue) / (mx - mn);
+            } else if(mx == rgb.green){
+                hsv.h = 60 * (rgb.blue - rgb.red) / (mx - mn) + 120;
+            } else {
+                hsv.h = 60 * (rgb.red - rgb.green) / (mx - mn) + 240;
+            }
+            if(hsv.h < 0) hsv.h += 360;
+        }
+    }
+    
+    return hsv;
 }
